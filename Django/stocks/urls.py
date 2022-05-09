0d793845900aac743509aa5961cbe5dad3ab344c
M. Brock Podgurski <mbpod10@gmail.com>
2022-05-09 17:01:27 -0400

    create routes that were the same as node

--- Django/stocks/urls.py
+++ Django/stocks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import stock_list
+from .views import stock_list, stock_list_offset, get_total_money
 
 urlpatterns = [
     path('stocks/', stock_list),
+    path('stocks/list/page/<int:page_num>/<str:order_query>/', stock_list_offset),
+    path('stocks/money/total/', get_total_money),
 ]
--- Django/stocks/views.py
+++ Django/stocks/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render
 from .models import Stock
 from .serializers import StockSerializer
@@ -6,6 +7,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Sum
 
 
 @api_view(['GET'])
@@ -14,3 +16,28 @@ def stock_list(request):
         stocks = Stock.objects.all()
         serializer = StockSerializer(stocks, many=True)
         return Response({"stocks": serializer.data})
+
+
+@api_view(['GET'])
+def stock_list_offset(request, page_num, order_query):
+    if request.method == 'GET':
+        pagination_count = 100
+        start = (page_num - 1) * pagination_count
+        end = page_num * pagination_count
+
+        if order_query == 'price' or order_query == 'amount':
+            stocks = Stock.objects.order_by(f"-{order_query}")[start: end]
+        else:
+            stocks = Stock.objects.order_by(order_query)[start: end]
+
+        serializer = StockSerializer(stocks, many=True)
+        return Response({"stocks": serializer.data})
+
+
+@api_view(['GET'])
+def get_total_money(request):
+    if request.method == 'GET':
+
+        total = Stock.objects.aggregate(Sum('price'))
+        return_response = {"total_amount": total['price__sum']}
+        return Response(return_response)
